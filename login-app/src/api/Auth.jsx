781085ce9ecc55fabe4e63720f4144fc1e83c97c
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-05-27 01:09:39 -0300

    feat: update api_url from backend.

--- login-app/src/api/Auth.jsx
+++ login-app/src/api/Auth.jsx
@@ -1,9 +1,10 @@
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://localhost:7214/api';
 
 export const login = async (credentials) => {
+  // eslint-disable-next-line no-useless-catch
   try {
-    const response = await fetch(`${API_URL}/auth/login`, {
+    const response = await fetch(`${API_URL}/Auth/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(credentials),
@@ -20,8 +21,9 @@ export const login = async (credentials) => {
 };
 
 export const register = async (userInfo) => {
+  // eslint-disable-next-line no-useless-catch
   try {
-    const response = await fetch(`${API_URL}/auth/register`, {
+    const response = await fetch(`${API_URL}/Auth/register`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(userInfo),
