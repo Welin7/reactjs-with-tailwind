6b0a806f159918852e8fae66ce4fc13dc9d5fda4
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-05-31 19:51:21 -0300

    feat: adjustment of descriptions.

--- login-app/src/components/Register.jsx
+++ login-app/src/components/Register.jsx
@@ -23,13 +23,13 @@ const Register = () => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-violet-500 to-white-400">
             <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
-                <h2 className="text-2xl font-semibold text-center text-black-800 mb-6">Criar Conta</h2>
+                <h2 className="text-2xl font-semibold text-center text-black-800 mb-6">Create Account</h2>
                 <form onSubmit={handleSubmit} className="space-y-4">
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Email</label>
                         <input
                             type="email"
-                            placeholder="seuemail@exemplo.com"
+                            placeholder="youremail@exemplo.com"
                             value={userName}
                             onChange={e => setUserName(e.target.value)}
                             required
@@ -37,7 +37,7 @@ const Register = () => {
                         />
                     </div>
                     <div>
-                        <label className="block text-sm font-medium text-gray-700">Senha</label>
+                        <label className="block text-sm font-medium text-gray-700">Password</label>
                         <input
                             type="password"
                             placeholder="••••••••"
@@ -52,7 +52,7 @@ const Register = () => {
                         type="submit"
                         className="w-full bg-violet-600 hover:bg-violet-700 text-white font-semibold py-2 rounded-lg transition duration-200"
                     >
-                        Cadastrar
+                        Register
                     </button>
                 </form>
             </div>
